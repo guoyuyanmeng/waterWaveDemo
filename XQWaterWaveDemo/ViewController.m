0bc297d931e2280b67314462fdae664cc4d2b3c9
guoyuyanmeng <kangxiaoqiangdesky@163.com>
2016-10-14 16:32:33 +0800

    Initial Commit

---
+++ XQWaterWaveDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XQWaterWaveDemo
+//
+//  Created by kang on 2016/10/14.
+//  Copyright © 2016年 kang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ XQWaterWaveDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XQWaterWaveDemo
+//
+//  Created by kang on 2016/10/14.
+//  Copyright © 2016年 kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
